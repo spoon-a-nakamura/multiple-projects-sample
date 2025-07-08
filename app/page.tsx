57d01ff849611902134277379424af7e040dabd0
Akifumi Nakamura <a-nakamura@studio-spoon.co.jp>
2025-07-08 14:48:04 +0900

    エラー修正

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Proxy Project</h1>
+      <p>This page should not be visible if rewrites are working correctly.</p>
+    </div>
+  )
+}
